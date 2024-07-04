21f6882be45cc793ce834b54133099808f12f913
JHKIM <156522742+HIJKM@users.noreply.github.com>
2024-07-04 19:22:22 +0900

    [Silver II] Title: 쇠막대기, Time: 60 ms, Memory: 31552 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,23 @@
+import sys
+
+iron_bars = sys.stdin.readline().rstrip()
+before, iron_bars = iron_bars[0], iron_bars[1:]
+stack = []
+bar_cnt = 0
+
+for i in iron_bars:
+    if before == '(':
+        if i == '(':
+            stack.append(before)
+        else: # i == ')'
+            bar_cnt += len(stack)
+    else: # before == ')'
+        if i == '(':
+            pass
+        else: # i == ')'
+            bar_cnt += 1
+            stack.pop(-1)
+    before = i
+
+print(bar_cnt)
+
